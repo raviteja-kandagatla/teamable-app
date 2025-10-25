b55bd30b04144c4bf66ec2f7ddf5102dbea80311
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-10-25 19:52:07 +0530

    add new changes to files

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ function handleEditProfile() {
 }
 
 function handleDisplayProfile() {
+    var updatedName = document.getElementById("input-name").value
+    var name = document.getElementById("name")
+    name.textContent = updatedName
+
+
     document.getElementById("display-view").style.display = "block"
     document.getElementById("edit-view").style.display = "none"
 }
