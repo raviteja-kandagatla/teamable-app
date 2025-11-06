7dc7a38e01cd99f39af677f326da3e8618ac2407
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-11-06 16:14:04 +0530

    add new changes to files index.html & app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 function handleEditProfile() {
-    new Datepicker('#birthday');
+  //  new Datepicker('#birthday'); //
     var name = document.getElementById("name").textContent
     var inputName = document.getElementById("input-name")
     inputName.value = name
 
     var email = document.getElementById("email").textContent
     var updatedEmail = document.getElementById("input-email")
-    if (validator.isEmail(updatedEmail.value)) {
-        email.textContent = updatedEmail
-    } else {
-        alert("wrong email format")
-    }
+    
+  //  if (validator.isEmail(updatedEmail.value)) {
+       // email.textContent = updatedEmail
+   // } else {
+    //    alert("wrong email format")
+   // } 
+    //
 
     var Interests = document.getElementById("Interests").textContent
     var inputInterests = document.getElementById("input-interest")
