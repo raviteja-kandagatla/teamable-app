03e61e7f37a9e63d63588238e4d8a2f5be4e9327
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-11-11 23:51:14 +0530

    fix: update vue files in teamable app

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="display-view">
+        <h1>User profile</h1>
+        <img src="profile.jpg">
+        
+        <p><b>Name:</b> <span id="name">Anna Smith</span></p>
+        <p><b>Email:</b> <span id="email">robustzig@example.com</span></p>
+        <p><b>Interests:</b> <span id="Interests">Coding</span></p>
+        <p><b>Birthday date:</b> <span id="birthday"></span></p> 
+
+        <button onclick="handleEditProfile()">Edit Profile</button>
+    </div>
+
+    <div id="edit-view">
+        <h1>User profile</h1>
+        <img src="profile.jpg">
+        
+        <span>Name: </span>
+        <input type="text" id="input-name" placeholder="Anna Smith"/>
+        <hr />
+
+        <span>Email: </span>
+        <input type="text" id="input-email" placeholder="robustzig@example.com"/>
+        <hr />
+
+        <span>Interests: </span>
+        <input type="text" id="input-interest" placeholder="coding"/>
+        <hr />
+        
+        <span>Birthday date: </span>
+        <input type="text" id="birthday" />
+        <hr /> 
+
+        <button onclick="handleDisplayProfile()">Display Profile</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+    }
+</script>
