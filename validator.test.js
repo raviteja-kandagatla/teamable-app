4c13e57cc364340d4349c2cde5a255d9092e3e28
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-11-29 22:31:21 +0530

    Update validator.js & validator.test.js applying unit tests to reflect new dependencies

--- validator.test.js
+++ validator.test.js
@@ -20,3 +20,18 @@ test('invalid email', function() {
     expect(result).toBe(true)
 })
 
+test('empty payload', function() {
+    const testPayload  = {}
+    const result = isEmptyPayload(testPayload)
+    expect(result).toBe(true)
+})
+
+test('non-empty payload', function() {
+    const testPayload  = {
+        name: "test name",
+        email: "test.email",
+        interests: "testing"
+    }
+    const result = isEmptyPayload(testPayload)
+    expect(result).toBe(false)
+})
